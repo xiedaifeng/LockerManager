8808108efa4b13f70eb6fdd5facb72766304dfaf
miotlink <pm@miotlinl.com>
2020-07-03 09:08:48 +0800

    application异步加载启动类

---
+++ app/src/main/java/com/locker/manager/task/LockerTask.java
@@ -0,0 +1,11 @@
+package com.locker.manager.task;
+
+import com.fmt.launch.starter.task.ITask;
+import com.fmt.launch.starter.task.Task;
+
+class LockerTask extends Task {
+    @Override
+    public void run() {
+
+    }
+}
