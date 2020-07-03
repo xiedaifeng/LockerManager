567e9bfc567b68264a6d5952e8a89c6b0c29c56f
hez_mac <428318>
2020-07-03 22:51:20 +0800

    串口

--- app/src/main/java/com/locker/manager/app/LockerApplication.java
+++ app/src/main/java/com/locker/manager/app/LockerApplication.java
@@ -1,5 +1,8 @@
 package com.locker.manager.app;
 
+import com.locker.manager.task.LockerTask;
+import com.qiao.launch.starter.TaskDispatcher;
+import com.qiao.launch.starter.task.Task;
 import com.yidao.module_lib.base.BaseApplication;
 
 public class LockerApplication extends BaseApplication {
@@ -7,5 +10,7 @@ public class LockerApplication extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        TaskDispatcher.init(this);
+        TaskDispatcher.createInstance().addTask(new LockerTask()).start();
     }
 }
---
+++ app/src/main/java/com/locker/manager/task/LockerTask.java
@@ -0,0 +1,10 @@
+package com.locker.manager.task;
+
+import com.qiao.launch.starter.task.Task;
+
+public class LockerTask extends Task {
+    @Override
+    public void run() {
+
+    }
+}
