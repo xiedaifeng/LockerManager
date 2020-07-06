65674aa07088281aafc4a6118372a0dd9bfccb5b
miotlink <pm@miotlinl.com>
2020-07-06 17:47:45 +0800

    调用串口SDK

--- app/src/main/java/com/locker/manager/app/LockerApplication.java
+++ app/src/main/java/com/locker/manager/app/LockerApplication.java
@@ -1,12 +1,22 @@
 package com.locker.manager.app;
 
+import android.content.SharedPreferences;
+
+import com.locker.manager.task.LockerTask;
+import com.qiao.launch.starter.TaskDispatcher;
 import com.yidao.module_lib.base.BaseApplication;
+
+
+
 
 public class LockerApplication extends BaseApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        TaskDispatcher.init(this);
+        TaskDispatcher.createInstance().addTask(new LockerTask()).start();
     }
+
+
 }
--- app/src/main/java/com/locker/manager/task/LockerTask.java
+++ app/src/main/java/com/locker/manager/task/LockerTask.java
@@ -1,11 +1,23 @@
 package com.locker.manager.task;
 
-import com.fmt.launch.starter.task.ITask;
-import com.fmt.launch.starter.task.Task;
 
-class LockerTask extends Task {
+import android.util.Log;
+
+import com.qiao.launch.starter.task.Task;
+import com.qiao.serialport.SerialPortOpenSDK;
+
+public class LockerTask extends Task {
     @Override
     public void run() {
+        try {
+            SerialPortOpenSDK.getInstance().initialize(mContext);
+            String [] strings=SerialPortOpenSDK.getInstance().getDevices();
+            Log.e("locker", strings.toString()+"---");
+            if (strings!=null&&strings.length>0){
 
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
