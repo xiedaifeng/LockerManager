555730cfc2111e54780d39a58a81b0cb3b97d8e0
hez_mac <428318>
2020-07-11 23:07:01 +0800

    串口

--- app/src/main/java/com/locker/manager/activity/SaveAppScanActivity.java
+++ app/src/main/java/com/locker/manager/activity/SaveAppScanActivity.java
@@ -1,5 +1,6 @@
 package com.locker.manager.activity;
 
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -7,8 +8,10 @@ import android.widget.TextView;
 
 import com.locker.manager.R;
 import com.locker.manager.base.BaseUrlView;
+import com.locker.manager.command.Command;
 import com.qiao.serialport.SerialPortOpenSDK;
 import com.qiao.serialport.listener.SerialPortMessageListener;
+import com.qiao.serialport.utils.ByteUtil;
 import com.yidao.module_lib.utils.ToastUtil;
 
 import butterknife.BindView;
@@ -32,29 +35,25 @@ public class SaveAppScanActivity extends BaseUrlView implements SerialPortMessag
 
     @Override
     public void init() {
-//        ivLeft.setVisibility(View.GONE);
-
-//        try {
-//            mInstance = SerialPortOpenSDK.getInstance();
-//            mInstance.setSerialPort("/dev/ttyS1",9600,1,8,0,0,0);
-//            SerialPortOpenSDK.getInstance().initialize(getCtx());
-//            String[] devices = SerialPortOpenSDK.getInstance().getDevices();
-//            for(String str:devices){
-//                Log.e("devices", str);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        ivLeft.setVisibility(View.VISIBLE);
     }
+
 
     @OnClick({R.id.iv_left, R.id.tv_hand_save,R.id.tv_help})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.iv_left:
+
                 try {
-                    SerialPortOpenSDK.getInstance().regirster(this);
-                    SerialPortOpenSDK.getInstance().send("57 4B 4C 59 08 01 86 86".replace(" ",""));
-                    SerialPortOpenSDK.getInstance().send("0B0040FF7A000B0000004933C3A36B".replace(" ",""));
+
+                    SerialPortOpenSDK.getInstance().send(
+                            new Command.Builder().setCommand(Command.Consts.COMMAND_WRITE).setCommmandChannel(Command.Consts.COMMAND_1).build().getBytes());
+
+                    SystemClock.sleep(500);
+
+
+                    SerialPortOpenSDK.getInstance().send(new Command.Builder().setCommand(Command.Consts.COMMAND_READ).setCommmandChannel(Command.Consts.COMMAND_1).build().getBytes());
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -69,7 +68,26 @@ public class SaveAppScanActivity extends BaseUrlView implements SerialPortMessag
     }
 
     @Override
-    public void onMessage(int error, String errorMessage, String data) throws Exception {
-        ToastUtil.showShortToast(errorMessage);
+    protected void onResume() {
+        SerialPortOpenSDK.getInstance().regirster(this);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        SerialPortOpenSDK.getInstance().unregirster(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onMessage(int error, String errorMessage, byte[] data) throws Exception {
+
+        /**
+         * 数据解析
+         */
+        Command command=new Command.Builder().setBytes(data).parse();
+        Log.e(command.getCommmandChannel(),command.getCommandState());
+
+
     }
 }
