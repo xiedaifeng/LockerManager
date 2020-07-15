a5cf7a8e49fa59f3144cb98605ae323dfa22de25
miotlink <pm@miotlinl.com>
2020-07-15 09:26:56 +0800

    修改组装包数据

--- app/src/main/java/com/locker/manager/activity/SaveAppScanActivity.java
+++ app/src/main/java/com/locker/manager/activity/SaveAppScanActivity.java
@@ -1,20 +1,18 @@
 package com.locker.manager.activity;
 
 
-import android.os.SystemClock;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.locker.manager.R;
 import com.locker.manager.base.BaseUrlView;
+import com.locker.manager.command.Command;
 import com.locker.manager.command.CommandProtocol;
 import com.qiao.serialport.SerialPortOpenSDK;
 import com.qiao.serialport.listener.SerialPortMessageListener;
 import com.yidao.module_lib.utils.DateUtils;
 import com.yidao.module_lib.utils.LogUtils;
-import com.yidao.module_lib.utils.ToastUtil;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -37,19 +35,18 @@ public class SaveAppScanActivity extends BaseUrlView implements SerialPortMessag
 
     @Override
     public void init() {
-//        ivLeft.setVisibility(View.GONE);
+    }
 
-//        try {
-//            mInstance = SerialPortOpenSDK.getInstance();
-//            mInstance.setSerialPort("/dev/ttyS1",9600,1,8,0,0,0);
-//            SerialPortOpenSDK.getInstance().initialize(getCtx());
-//            String[] devices = SerialPortOpenSDK.getInstance().getDevices();
-//            for(String str:devices){
-//                Log.e("devices", str);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SerialPortOpenSDK.getInstance().unregirster(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SerialPortOpenSDK.getInstance().regirster(this);
     }
 
     @OnClick({R.id.iv_left, R.id.tv_hand_save,R.id.tv_help})
@@ -57,13 +54,13 @@ public class SaveAppScanActivity extends BaseUrlView implements SerialPortMessag
         switch (view.getId()) {
             case R.id.iv_left:
                 try {
-
-                    SerialPortOpenSDK.getInstance().send( new CommandProtocol.Builder().setCommand("21").setCommandChannel("01").builder().getBytes());
-
-                    SystemClock.sleep(1000);
-                   SerialPortOpenSDK.getInstance().send( new CommandProtocol.Builder().setCommand("22").setCommandChannel("01").builder().getBytes());
-                    SystemClock.sleep(1000);
-                    SerialPortOpenSDK.getInstance().send( new CommandProtocol.Builder().setCommand("25").setCommandChannel("01").builder().getBytes());
+                    new CommandProtocol.Builder().setCommand(CommandProtocol.COMMAND_OPEN).setCommandChannel(5).builder();
+                    SerialPortOpenSDK.getInstance().send(
+                            new CommandProtocol.Builder()
+                                    .setCommand(CommandProtocol.COMMAND_OPEN)
+                                    .setCommandChannel(1)
+                                    .builder()
+                                    .getBytes());
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -78,20 +75,12 @@ public class SaveAppScanActivity extends BaseUrlView implements SerialPortMessag
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        SerialPortOpenSDK.getInstance().regirster(this);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        SerialPortOpenSDK.getInstance().unregirster(this);
-    }
-
-    @Override
     public void onMessage(int error, String errorMessage, byte[] data) throws Exception {
-      CommandProtocol commandProtocol=new CommandProtocol.Builder().setBytes(data).parseMessage();
-        Log.e("--",commandProtocol.toString());
+        Command command=new Command.Builder().setBytes(data).parse();
+        if (command.getError()==0x01&&command.getCommandFixed_1().equals("30")){
+           String channel= command.getCommmandChannel();
+           String state=command.getCommandState();
+        }
+
     }
 }
