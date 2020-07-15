7eaf28fb9dc9a2671de1f30cc87b56088fde2001
miotlink <pm@miotlinl.com>
2020-07-15 09:14:51 +0800

    修改组装包数据

--- SerialPort/src/main/java/com/qiao/serialport/utils/ByteUtil.java
+++ SerialPort/src/main/java/com/qiao/serialport/utils/ByteUtil.java
@@ -2,6 +2,9 @@ package com.qiao.serialport.utils;
 
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ByteUtil {
     public static int isOdd(int num) {
         return num & 0x1;
@@ -61,5 +64,236 @@ public class ByteUtil {
 //        Log.e("res",sum+"");
 
         return result;
+    }
+
+    /**
+     * int 转2 个byte
+     * @param numInt
+     * @return
+     */
+    public static byte[] intToTwoByte(int numInt){
+        byte[] rest = new byte[2];
+        if(numInt < -32768 || numInt > 32767){
+            return null;
+        }
+        rest[0] = (byte)(numInt >> 8);//高8位
+        rest[1] = (byte)(numInt & 0x00ff);//低8位
+        return rest;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    public static byte[] merge(byte[] bytes1, byte[] bytes2) {
+        byte[] ret = new byte[bytes1.length + bytes2.length];
+        System.arraycopy(bytes1, 0, ret, 0, bytes1.length);
+        System.arraycopy(bytes2, 0, ret, bytes1.length, bytes2.length);
+        return ret;
+    }
+
+    public static byte getXor(byte[] bytes, int len){
+        byte ret=0;
+        for (int i = 0; i <len; i++) {
+            ret ^=bytes[i];
+        }
+
+        return ret;
+    }
+    /**
+     * 字节数组转换成对应的16进制表示的字符串
+     *
+     * @param src
+     * @return
+     */
+    public static String bytes2HexStr(byte[] src) {
+        StringBuilder builder = new StringBuilder();
+        if (src == null || src.length <= 0) {
+            return "";
+        }
+        char[] buffer = new char[2];
+        for (int i = 0; i < src.length; i++) {
+            buffer[0] = Character.forDigit((src[i] >>> 4) & 0x0F, 16);
+            buffer[1] = Character.forDigit(src[i] & 0x0F, 16);
+            builder.append(buffer);
+        }
+        return builder.toString().toUpperCase();
+    }
+
+    /**
+     * 十六进制字节数组转字符串
+     *
+     * @param src 目标数组
+     * @param dec 起始位置
+     * @param length 长度
+     * @return
+     */
+    public static String bytes2HexStr(byte[] src, int dec, int length) {
+        byte[] temp = new byte[length];
+        System.arraycopy(src, dec, temp, 0, length);
+        return bytes2HexStr(temp);
+    }
+
+    /**
+     * 16进制字符串转10进制数字
+     *
+     * @param hex
+     * @return
+     */
+    public static long hexStr2decimal(String hex) {
+        return Long.parseLong(hex, 16);
+    }
+
+    /**
+     * 把十进制数字转换成足位的十六进制字符串,并补全空位
+     *
+     * @param num
+     * @return
+     */
+    public static String decimal2fitHex(long num) {
+        String hex = Long.toHexString(num).toUpperCase();
+        if (hex.length() % 2 != 0) {
+            return "0" + hex;
+        }
+        return hex.toUpperCase();
+    }
+
+    /**
+     * 把十进制数字转换成足位的十六进制字符串,并补全空位
+     *
+     * @param num
+     * @param strLength 字符串的长度
+     * @return
+     */
+    public static String decimal2fitHex(long num, int strLength) {
+        String hexStr = decimal2fitHex(num);
+        StringBuilder stringBuilder = new StringBuilder(hexStr);
+        while (stringBuilder.length() < strLength) {
+            stringBuilder.insert(0, '0');
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String fitDecimalStr(int dicimal, int strLength) {
+        StringBuilder builder = new StringBuilder(String.valueOf(dicimal));
+        while (builder.length() < strLength) {
+            builder.insert(0, "0");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 字符串转十六进制字符串
+     *
+     * @param str
+     * @return
+     */
+    public static String str2HexString(String str) {
+        char[] chars = "0123456789ABCDEF".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        byte[] bs = null;
+        try {
+
+            bs = str.getBytes("utf8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int bit;
+        for (int i = 0; i < bs.length; i++) {
+            bit = (bs[i] & 0x0f0) >> 4;
+            sb.append(chars[bit]);
+            bit = bs[i] & 0x0f;
+            sb.append(chars[bit]);
+        }
+        return sb.toString();
+    }
+    public static byte[] time2HexBytes(Date date) {
+//        String dtStr = new SimpleDateFormat("ssmmHHddMMyy").format(new Date());
+        String dtStr = new SimpleDateFormat("yyMMddHHmmss").format(date);
+        //    dtStr="002006180298";
+        // System.out.println(dtStr);
+        byte[] tmp = ByteUtil.decStr2bytes(dtStr);
+        return tmp;
+    }
+
+    /**
+     * 把十六进制表示的字节数组字符串，转换成十六进制字节数组
+     *
+     * @param
+     * @return byte[]
+     */
+    public static byte[] decStr2bytes(String dec) {
+        int len = (dec.length() / 2);
+        byte[] result = new byte[len];
+        char[] achar = dec.toUpperCase().toCharArray();
+        for (int i = 0; i < len; i++) {
+            int pos = i * 2;
+            result[i] = (byte) (hexChar2byte(achar[pos]) * 10 + hexChar2byte(achar[pos + 1]));
+        }
+        return result;
+    }
+    /**
+     * 把十六进制表示的字节数组字符串，转换成十六进制字节数组
+     *
+     * @param
+     * @return byte[]
+     */
+    public static byte[] hexStr2bytes(String hex) {
+        int len = (hex.length() / 2);
+        byte[] result = new byte[len];
+        char[] achar = hex.toUpperCase().toCharArray();
+        for (int i = 0; i < len; i++) {
+            int pos = i * 2;
+            result[i] = (byte) (hexChar2byte(achar[pos]) << 4 | hexChar2byte(achar[pos + 1]));
+        }
+        return result;
+    }
+
+    /**
+     * 把16进制字符[0123456789abcde]（含大小写）转成字节
+     *
+     * @param c
+     * @return
+     */
+    private static int hexChar2byte(char c) {
+        switch (c) {
+            case '0':
+                return 0;
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':
+                return 4;
+            case '5':
+                return 5;
+            case '6':
+                return 6;
+            case '7':
+                return 7;
+            case '8':
+                return 8;
+            case '9':
+                return 9;
+            case 'a':
+            case 'A':
+                return 10;
+            case 'b':
+            case 'B':
+                return 11;
+            case 'c':
+            case 'C':
+                return 12;
+            case 'd':
+            case 'D':
+                return 13;
+            case 'e':
+            case 'E':
+                return 14;
+            case 'f':
+            case 'F':
+                return 15;
+            default:
+                return -1;
+        }
     }
 }
